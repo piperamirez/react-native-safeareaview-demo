a6b51e1c4c7991ddff1f84418be514c7d0796cdf
Felipe Ramirez <framirez@thoughtworks.com>
2018-05-04 15:56:08 -0300

    Added stack navigator at root level

--- app/App.js
+++ app/App.js
@@ -3,30 +3,18 @@ import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 import * as theme from './style/theme'
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <SafeAreaView style={styles.app}>
-                <StatusBar
-                    backgroundColor="black"
-                    barStyle="light-content"/>
-                <View style={styles.view}>
-                    <Text>Yay! 🎉</Text>
-                </View>
-            </SafeAreaView>
-        )
-    }
-}
+import HomeScreen from './Home/HomeScreen'
 
-const styles = StyleSheet.create({
-    app: {
-        flex: 1,
-        backgroundColor: theme.primary,
-    },
-    view: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
+import { StackNavigator } from 'react-navigation'
+
+const App = StackNavigator({
+    Home: { screen: HomeScreen }
+},
+{
+    headerMode: 'none',
+    navigationOptions: {
+        headerVisible: false,
     }
-});
+})
+
+export default App
---
+++ app/Home/HomeScreen.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+import * as theme from '../style/theme'
+
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome'
+    }
+    render() {
+        return (
+            <SafeAreaView style={styles.app}>
+                <StatusBar
+                    backgroundColor="black"
+                    barStyle="light-content"/>
+                <View style={styles.view}>
+                    <Text>Yay! 🎉</Text>
+                </View>
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    app: {
+        flex: 1,
+        backgroundColor: theme.primary,
+    },
+    view: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
